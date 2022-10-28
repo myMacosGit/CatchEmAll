607d630343c48c374e292762e56a99c06f8e5435
myMacosGit <reg.isaacs@gmail.com>
2022-10-28 11:17:36 +0200

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Richard Isaacs on 28.10.22.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
